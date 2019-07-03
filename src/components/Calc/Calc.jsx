890e429eda0fa92664a27209f377d66191d5b289
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-07-03 18:57:05 -0500

    got the calculator making expressions...getting there

--- src/components/Calc/Calc.jsx
+++ src/components/Calc/Calc.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Grid} from '@material-ui/core';
+import {Grid, Button} from '@material-ui/core';
 import socketIOClient from 'socket.io-client';
 import Axios from 'axios';
 
@@ -9,7 +9,6 @@ import './Calc.css';
 import ExpressionDisplay from '../ExpressionDisplay/ExpressionDisplay.jsx';
 
 const URL = `ws://localhost:${process.env.port || 5000}`
-// const ws = new WebSocket(URL)
 
 class Calc extends Component{
 
@@ -17,43 +16,14 @@ class Calc extends Component{
         test: '',
         expressions: []
     }    
-    
-    // ws = new WebSocket(URL);  
+        
     socket = socketIOClient(URL);  
 
     componentDidMount() {
-
         this.socket.on('sendExpression', (expression) => {
             console.log('got the expression back:', expression)
             this.addExpression(expression);
         })
-
-        // this.socket.on('()' => {
-        //     console.log('connected');
-        //     this.socket.emit('testing connection');
-        // }
-
-        // console.log('mount');
-        // this.ws.onopen = () => {
-        //     //on connecting, log
-        //     console.log('connected');
-        //     this.ws.send('hello');
-        // }
-
-        // this.ws.onmessage = (event) => {
-        //     //on receiving an expression, add it to  state
-        //     console.log('event.data', event.data);
-        //     const expression = JSON.parse(event.data);
-        //     this.addExpression(expression);
-        // }
-
-        // this.ws.onclose = () => {
-        //     console.log('disconnected');
-        //     //attempt to reconnect on loss of connection
-        //     this.setState({
-        //         ws: new WebSocket(URL),
-        //     })
-        // }
     }
 
     addExpression = (expression) => {
@@ -71,10 +41,7 @@ class Calc extends Component{
 
     handleSubmit = (event) => {
         event.preventDefault();
-        const message = this.state.test;
-        // Axios.post('/echo', message);        
-        // this.ws.send(JSON.stringify(message));
-        // this.addExpression(message);
+        const message = this.state.test;        
         this.socket.emit('sendExpression', message);
     }
 
@@ -85,18 +52,8 @@ class Calc extends Component{
         return(
             <div className="calcDiv">
                 <Grid container justify="center">
-                    <Grid item xs={12}>
-
-                        <Grid item xs={10}>
-                            {/* in progress expression here */}
-                            <input onChange={this.handleChange} />
-                            <button onClick={this.handleSubmit}>send test</button>
-                        </Grid>
-                        <Grid item xs={2}>
-                            {/* put the C here */}
-                        </Grid>
-
-                        <CalcButtons />
+                    <Grid item xs={8}>
+                        <CalcButtons handleChange={this.handleChange}/>
                         <ExpressionDisplay expressions={this.state.expressions}/>
                     </Grid>
 
--- src/components/Calc/CalcButtons.jsx
+++ src/components/Calc/CalcButtons.jsx
@@ -5,63 +5,111 @@ import './Calc.css';
 
 class CalcButtons extends Component{
 
+    state = {
+        currentNumber: 0,
+        currentExpression: '',
+        numbers: [],
+        operands: [],
+    }
+
     handleClick = (event) => {
         console.log('button value:', event.currentTarget.value);
+        this.setState({
+            ...this.state,
+            currentExpression: this.state.currentExpression + event.currentTarget.value,
+            currentNumber: this.state.currentNumber + event.currentTarget.value,
+        })
+    }
+
+    resolveExpression = () => {
+        console.log("resolving expression:", this.state.currentExpression);
     }
 
     handleOperation = (operation) => {
+        let currentNumber = this.state.currentNumber;
+        let operand = '';
+
         switch (operation) {
             case 'add':
                 console.log('adding');
+                operand = '+';
                 break;
             case 'subtract':
                 console.log('subtracting');
+                operand = '-';
                 break;
             case 'divide':
                 console.log('dividing');
+                operand = '/';
                 break;
             case 'multiply':
                 console.log('multiply');
+                operand = '*';
                 break;
             case 'resolve':
                 console.log('resolving');
+                this.resolveExpression();
                 break;
         
             default:
                 break;
         }
+
+        this.setState({
+            ...this.state,
+            numbers: [...this.state.numbers, Number(this.state.currentNumber)],
+            operands: [...this.state.operands, operand],
+            currentNumber: '0',
+            currentExpression: this.state.currentExpression + ' ' + operand + ' ',
+        });
+    }
+
+    clearExpression = () => {
+        this.setState({
+            currentExpression: '',
+            currentNumber: 0,
+            numbers: [],
+            operands: []
+        })
     }
 
     render(){
+        console.log("calc button state:", this.state);
         return(
             <Grid container justify="center">
-                <Grid item xs={8}>
+                <Grid item xs={12}>
+                    <Grid item xs={12}>
+                        <input disabled value={this.state.currentExpression} className="inputField" onChange={this.props.handleChange} />
+                        <Button className="cButton" variant="contained" color="primary" onClick={this.clearExpression}>C</Button>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
                     <Grid item xs={12}>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="7">7</Button>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="8">8</Button>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="9">9</Button>
-                        <Button className="calcButton" variant='contained' onClick={(e) => this.handleOperation('divide')} value="÷">÷</Button>
+                        <Button className="calcButton" variant='contained' color="primary" onClick={(e) => this.handleOperation('divide')} value="÷">÷</Button>
                     </Grid>
 
                     <Grid item xs={12}>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="4">4</Button>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="5">5</Button>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="6">6</Button>
-                        <Button className="calcButton" variant='contained' onClick={(e) => this.handleOperation('multiply')} value="×">×</Button>
+                        <Button className="calcButton" variant='contained' color="primary" onClick={(e) => this.handleOperation('multiply')} value="×">×</Button>
                     </Grid>
 
                     <Grid item xs={12}>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="1">1</Button>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="2">2</Button>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="3">3</Button>
-                        <Button className="calcButton" variant='contained' onClick={(e) => this.handleOperation('subtract')} value="-">-</Button>
+                        <Button className="calcButton" variant='contained' color="primary" onClick={(e) => this.handleOperation('subtract')} value="-">-</Button>
                     </Grid>
 
                     <Grid item xs={12}>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value="0">0</Button>
                         <Button className="calcButton" variant='contained' onClick={this.handleClick} value=".">.</Button>
-                        <Button className="calcButton" variant='contained' onClick={(e) => this.handleOperation('resolve')} value="=">=</Button>
-                        <Button className="calcButton" variant='contained' onClick={(e) => this.handleOperation('add')} value="+">+</Button>
+                        <Button className="calcButton" variant='contained' color="primary" onClick={(e) => this.handleOperation('resolve')} value="=">=</Button>
+                        <Button className="calcButton" variant='contained' color="primary" onClick={(e) => this.handleOperation('add')} value="+">+</Button>
                     </Grid>
                 </Grid>
             </Grid>            
