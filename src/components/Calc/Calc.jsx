46eb817e1871034f49d91d82d20e17d7712995f2
Stefen Menzel <stuff@Stefens-MBP.lan>
2019-07-04 14:21:01 -0500

    sockets and heroku aren't friends

--- src/components/Calc/Calc.jsx
+++ src/components/Calc/Calc.jsx
@@ -10,10 +10,10 @@ import ExpressionDisplay from '../ExpressionDisplay/ExpressionDisplay.jsx';
 
 // const URL = `ws://localhost:${process.env.port || 5000}`
 // const URL = `ws:https://immense-dawn-65811.herokuapp.com:${process.env.port || 5000}`
-// const URL = (process.env.port) ? 
-//     `https://immense-dawn-65811.herokuapp.com/` :
-//     `ws://192.168.1.135:5000`;
-let URL = window.location.origin.replace(/^https/, 'ws');
+const URL = (process.env.port) ? 
+    `https://immense-dawn-65811.herokuapp.com/` :
+    `ws://192.168.1.135:5000`;
+// let URL = window.location.origin.replace(/^https/, 'ws');
 // if(process.env.port){
 //     URL = window.location.origin.replace(/^http/, 'ws');
 // }else{
