965a9df260680c23a8f91c341abd0e9211430e69
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-07-05 16:01:46 -0500

    got heroku working

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const socketIO = require('socket.io');
 const http = require('http');
 
-// const SocketServer = require('ws').Server;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -18,52 +17,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //serve static files
 app.use(express.static('build'));
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// })
 
 server.listen(PORT, () => console.log("server is running on:", PORT));
-
-
-// const server = express()
-//     .use((req, res) => res.sendFile(INDEX))
-//     .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-// app.get('/', (req, res, next) => res.sendFile(__dirname + './index.html'));
-// io.configure(() => {
-//     io.set("transports", ["xhr-polling"]);
-//     io.set("polling duration", 10);
-// })
-//app set
-
-// const INDEX = path.join(__dirname, 'index.html');
-
-// const server = express()
-//     .use((req, res) => res.sendFile(INDEX))
-//     .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-// const wss = new SocketServer({server});
-
-// wss.on('connection', (ws) => {
-//     console.log('user connected:');
-//     ws.on('message', (data) => {
-//         console.log('received message:', data);
-//         wss.clients.forEach((client) => {
-//             if(client.readyState === WebSocket.OPEN){
-//                 console.log('sending back');
-//                 client.send(data);
-//             }
-//         })
-//     })    
-//     ws.on('close', () => {
-//         console.log('user disconnected');
-//     })
-// });
-//listen
-// server.listen(PORT, () => {
-//     console.log(`Listening on port: ${PORT}`);
-// })
 
 io.sockets.on('connection', socket => {
     // io.emit('hello', {message: 'hello from server!'});
@@ -75,17 +30,4 @@ io.sockets.on('connection', socket => {
         console.log('expression received:', expression);
         io.emit('sendExpression', expression);
     })
-    // socket.on('message', (data) => {
-    //     console.log('server got a web socket message:', data);
-    //     wss.clients.forEach((client) => {
-    //         if (client !== ws && client.readyState === WebSocket.OPEN) {
-    //             client.send(data);
-    //         }
-    //     })
-    // })
 })
-
-
-// setInterval(() => {
-//     io.emit('time', new Date().toTimeString())
-// }, 1000);
--- src/components/Calc/Calc.jsx
+++ src/components/Calc/Calc.jsx
@@ -8,30 +8,8 @@ import CalcButtons from './CalcButtons.jsx';
 import './Calc.css';
 import ExpressionDisplay from '../ExpressionDisplay/ExpressionDisplay.jsx';
 
-// const URL = `ws://localhost:${process.env.port || 5000}`
-// const URL = `ws:https://immense-dawn-65811.herokuapp.com:${process.env.port || 5000}`
-// const URL = (process.env.PORT) ? 
-//     `https://immense-dawn-65811.herokuapp.com/` :
-//     `ws://192.168.1.135:5000`;
 const URL = `https://alluring-mesa-verde-82256.herokuapp.com`;
-// const HOST = window.location.origin.replace(/^http/, 'ws');
-const HOST = `ws://192.168.1.135:5000`;
-console.log('host:', HOST);
-console.log("host:", `ws://${HOST.hostname}:${process.env.PORT || 5000}`);
-console.log('process.env.PORT is real:', (process.env.PORT) ? true : false);
 
-// let URL = window.location.origin.replace(/^https/, 'ws');
-// if(process.env.port){
-//     URL = window.location.origin.replace(/^http/, 'ws');
-// }else{
-//     URL = `ws://localhost:5000`; 
-// }
-
-// const socket = socketIOClient(URL, {
-//     secure: true,
-//     transports: ['websocket'],
-//     upgrade: false,
-// });
 const socket = socketIOClient(URL);
 
 class Calc extends Component{
@@ -40,39 +18,8 @@ class Calc extends Component{
         test: '',
         expressions: []
     }    
-        
-    // socket = socketIOClient(URL);  
-    // socket = io();
-    // socket = socketIOClient.connect(URL, {
-    //     transports: ['websocket'],
-    //     upgrade: false,
-    // })
-    // socket = socketIOClient(URL, {secure: true});    
-    // socket = socketIOClient(URL, {
-    //     transports: ['websocket'],
-    //     jsonp: false
-    // });    
-    // socket = new WebSocket(HOST);
 
     componentDidMount() {
-        // this.socket.onopen = () => {
-        //     console.log("connected to server");
-        // }
-        // this.socket.onmessage = (expression) => {
-        //     console.log('got an expression from the server:', expression.data);
-        //     const message = JSON.parse(expression.data);
-        //     console.log('message from server:', message);
-        //     this.addExpression(message);
-        // }
-        // this.socket.onclose = () => {
-        //     console.log("disconnected");
-        //     this.setState({
-        //         socket: new WebSocket(URL)
-        //     })
-        // }
-
-        // const socket = socketIOClient(URL, {transports: ['websocket']});
-        // socket.connect();
         socket.on('connection', () => {
             console.log('connected to server');
         })
@@ -97,10 +44,7 @@ class Calc extends Component{
     }
 
     handleSubmit = (expression) => {
-        // event.preventDefault();
         const message = {expression: expression};
-        // this.socket.send(message);
-        // this.socket.send(JSON.stringify(message));
         socket.emit('sendExpression', message);
     }
 
