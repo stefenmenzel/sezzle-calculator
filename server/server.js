e9ec77006b110ab32eda8e2978dc5f1a47935869
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-07-05 14:52:13 -0500

    dkjfkjkjdkfk

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //serve static files
 // app.use(express.static('build'));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
