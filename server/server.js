f93af3b85da68daa838f26a5ed9f53f6364d097c
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-07-05 15:41:41 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO.listen(server);
+
 
 const bodyParser = require('body-parser');
 
@@ -17,11 +17,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //serve static files
-// app.use(express.static('build'));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+app.use(express.static('build'));
+const io = socketIO.listen(server);
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// })
 
 // server.listen(PORT, () => console.log("server is running on:", PORT));
 
